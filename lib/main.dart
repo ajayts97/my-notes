66f49d6d358e76a7d1142afd833e7d89ba90eb1f
AJAY T S <ajayts2697@gmail.com>
2022-09-06 14:06:21 +0530

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
-// import 'package:mynotes/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 
 import 'firebase_options.dart';
 
@@ -21,6 +17,7 @@ void main() {
     routes: {
       '/login/': (context) => const LoginView(),
       '/register/': (context) => const RegisterView(),
+      '/notes/': (context) => const NotesView(),
     },
   ));
 }
@@ -78,8 +75,9 @@ class _NotesViewState extends State<NotesView> {
                   final shouldLogout = await showLogoutDialog(context);
                   if (shouldLogout) {
                     await FirebaseAuth.instance.signOut();
-                    final navigator = Navigator.of(context);
-                    navigator.pushNamedAndRemoveUntil('/login/', (_) => false);
+                    // final navigator = Navigator.of(context);
+                    // ignore: use_build_context_synchronously
+                    Navigator.of(context).pushNamedAndRemoveUntil('/login/', (_) => false);
                   }
                   break;
                 default:
